3290fa1cc85ae834ac4e47eeb21bdb136020f000
MainShayne233 <shaynetremblay@gmail.com>
2020-04-11 22:50:35 -0400

    Get running

---
+++ frontend/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
+
+const crateDirectory = path.resolve(__dirname, "../ltcs_compiler");
+const outDir = path.resolve(__dirname, "pkg");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new WasmPackPlugin({
+      crateDirectory,
+      outDir,
+    }),
+    // Have this example work in Edge which doesn't ship `TextEncoder` or
+    // `TextDecoder` at this time.
+    new webpack.ProvidePlugin({
+      TextDecoder: ["text-encoding", "TextDecoder"],
+      TextEncoder: ["text-encoding", "TextEncoder"],
+    }),
+  ],
+  mode: "development",
+};
